007f351a64227543d14cd511103ff05bb9c2ef0e
LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>
2023-04-24 00:33:00 +0530

    Update play.py

--- AsuXMusic/Modules/play.py
+++ AsuXMusic/Modules/play.py
@@ -160,7 +160,7 @@ async def play(c: Abishnoi, m: Message):
                     [
                         [
                             InlineKeyboardButton(
-                                "• sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/AbishnoiMF"
+                                "• sᴜᴘᴘᴏʀᴛ", url=f"https://t.me/DXinfo143"
                             ),
                             InlineKeyboardButton("• ᴄʟᴏsᴇ", callback_data="cls"),
                         ]
